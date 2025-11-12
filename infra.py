284ef36b6d3ae5a4c999a25e33b0e033c8acf0b8
Mazin Karjikar <mazkar3@gmail.com>
2025-11-12 16:20:42 -0500

    initial commit with infrastructure file

---
+++ infra.py
@@ -0,0 +1,14 @@
+import concurrent.futures
+import json
+import re
+from openai import OpenAI
+
+# vllm setup
+openai_api_key = "EMPTY"
+openai_api_base = "http://localhost:8000/v1"
+
+client = OpenAI(
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+
